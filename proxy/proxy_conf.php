e75277dbdf43625e3976f5e67bee7ec776d38bfc
Phuker <11596611+Phuker@users.noreply.github.com>
2016-07-21 11:03:33 +0800

    update beautyFunc
    beautification.
    modified the form to make it work well.

--- proxy/proxy_conf.php
+++ proxy/proxy_conf.php
@@ -1,4 +1,5 @@
 <?php
+// 上层代理
 $baiduConf = ['url'=>'https://www.baidu.com/s?ie=utf-8&rn=20&wd=',
 	'proxy'=>false,
 	'proxyHost'=>'',
@@ -6,6 +7,8 @@ $baiduConf = ['url'=>'https://www.baidu.com/s?ie=utf-8&rn=20&wd=',
 	'sslCert'=>getcwd() . "/cert/baidu.com.crt",
 	'beautyFunc'=>'baiduBeauty'];
 function baiduBeauty($html){
+	$html = str_replace('<form id="form" name="f" action="/s" class="fm">', '<form id="form" name="f" action="" class="fm"><input type="hidden" name="engine" value="baidu">', $html);
+	$html = str_replace('<input id="kw" name="wd" class="s_ipt" value="', '<input id="kw" name="q" class="s_ipt" value="', $html);
 	return str_replace('<div id="content_left">',  '<div id="content_left" style="padding-left:12px;">', $html);
 }
 
@@ -15,8 +18,10 @@ $googleConf = ['url'=>'https://www.google.com/search?site=webhp&source=hp&newwin
 	'proxyPort'=>8080,
 	'sslCert'=>getcwd() . "/cert/google.com.crt",
 	'beautyFunc'=>'googleBeauty'];
-	
+
 function googleBeauty($html){
+	$html = str_replace('<form class="tsf" action="/search" style="overflow:visible" id="tsf" method="GET" name="f" onsubmit="return q.value!=\'\'" role="search">', '<form class="tsf" action="" style="overflow:visible" id="tsf" method="GET" name="f" onsubmit="return q.value!=\'\'" role="search"><input type="hidden" name="engine" value="google">', $html);
+	$html = str_replace('<div class="sbibtd">', '<div class="sbibtd" style="width:500px;">', $html);
 	return str_replace('<div id="center_col">', '<div id="center_col" style="margin:0;">', $html);
 }
 
