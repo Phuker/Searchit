e51e77a96e94ebfd5d0cdd51406b0f3a2dc7cbbd
Phuker <11596611+Phuker@users.noreply.github.com>
2016-07-24 17:51:13 +0800

    自适应宽度
    宽度过小（700px）同时只显示一个iframe，并显示切换按钮。

--- search.php
+++ search.php
@@ -25,44 +25,99 @@ $keyword_link = urlencode($_GET[$getArg]);
 ?>
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title><?php echo $keyword; ?>_<?php echo $siteName; ?></title>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/vnd.microsoft.icon">
-        <link rel="icon" href="/favicon.ico" type="image/vnd.microsoft.icon">
-        <style type="text/css">
-		    html{
-		    	height:100%;
-		    }
-		    body{
-		    	height:100%;
-		    	margin:0px;
-		    }
-		    .head{
-		    	background-color:#F1F1F1;
-		    	min-height: 3%;
-		    	padding:2px;
-		    	margin: 0px;
-		    }
-		    iframe{
-		    	height:96%;
-		    	margin: 0px;
-		    	border:0px;
-		    }
-		</style>
-    </head>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<title><?php echo $keyword; ?>_<?php echo $siteName; ?></title>
+<link rel="shortcut icon" href="/favicon.ico" type="image/vnd.microsoft.icon">
+<link rel="icon" href="/favicon.ico" type="image/vnd.microsoft.icon">
+<style type="text/css">
+html{
+	height:100%;
+}
+body{
+	height:100%;
+	margin:0px;
+}
+.head{
+	background-color:#F1F1F1;
+	height: 5%;
+	padding:2px;
+	margin: 0px;
+}
 
-    <body>
-    <div class="head">
-	    <center>
-	    <form action="" method="GET" target=_parent>
-		    <input type="text" id="<?php echo $getArg; ?>" name="<?php echo $getArg; ?>" size="38" maxlength="255" value="<?php echo $keyword; ?>"/>
-		    <input type="submit" value="Search It!"/>
-	    </form>
-	    </center>
-    </div>
-    <div style="clear:both;"></div>
-    <iframe src="<?php echo $baiduurl . $keyword_link; ?>" name="popmain" style="width:51%;float:left;" bordercolor="#3E77B3"></iframe>
-    <iframe src="<?php echo $googleurl . $keyword_link; ?>" name="popforum" style="width:48%;float:right;" bordercolor="#3E77B3"></iframe>
-    </body>
+@media (max-width: 700px){
+	iframe{
+		height:89%;
+		margin: 0px;
+		border:0px;
+	}
+	#iframeLeft{
+		width:0%;
+		float:left;
+	}
+	#iframeRight{
+		width:100%;
+		float:right;
+	}
+
+	#switchBtn{
+		display: block;
+		width: 100%;
+		height: 2.2em;
+		margin:0;
+	}
+}
+
+@media (min-width: 700px){
+	iframe{
+		height:94%;
+		margin: 0px;
+		border:0px;
+	}
+	#iframeLeft{
+		width:51% !important;
+		float:left;
+	}
+	#iframeRight{
+		width:48% !important;
+		float:right;
+	}
+	#switchBtn{
+		display: none;
+	}
+}
+</style>
+</head>
+
+<body>
+<div class="head">
+    <center>
+    <form action="" method="GET" target=_parent>
+	    <input type="text" id="<?php echo $getArg; ?>" name="<?php echo $getArg; ?>" style="width:75%;height:1.6em;" value="<?php echo $keyword; ?>"/>
+	    <input type="submit" value="Search It!"/>
+    </form>
+    </center>
+</div>
+<iframe src="<?php echo $baiduurl . $keyword_link; ?>" id="iframeLeft" bordercolor="#3E77B3"></iframe>
+<iframe src="<?php echo $googleurl . $keyword_link; ?>" id="iframeRight" bordercolor="#3E77B3"></iframe>
+<script type="text/javascript">
+var currentRight;
+var currentRight = true;
+function Switch(){
+	iframeLeft = document.getElementById('iframeLeft');
+	iframeLeft = document.getElementById('iframeLeft')
+	iframeRight = document.getElementById('iframeRight');
+	if(currentRight){
+		iframeRight.style['width'] = "0%";
+		iframeLeft.style['width'] = "100%";
+	} else {
+		iframeRight.style['width'] = "100%";
+		iframeLeft.style['width'] = "0%";
+	}
+	currentRight = !currentRight;
+}
+</script>
+<button onclick="Switch();" id="switchBtn">Switch</button>
+</body>
 </html>
