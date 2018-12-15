dd3dc6401a7490bab2b6ee5457e161c59476f97d
Phuker <11596611+Phuker@users.noreply.github.com>
2018-12-16 01:06:29 +0800

    proxy.php 防呆设计
    搜索框不允许点击
    其他更改

--- www/config.php
+++ www/config.php
@@ -9,37 +9,43 @@ $googleurl='./proxy/proxy.php?engine=google&q='; //
 $baiduurl='./proxy/proxy.php?engine=baidu&q=';
 
 // 上层代理
-$baiduConf = ['url'=>'https://www.baidu.com/s?ie=utf-8&rn=20&wd=',
-			'proxy'=>false,
-			'beautyFunc'=>'baiduBeauty',
-			'userAgent'=>'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'];
+$baiduConf = [
+	'url'=>'https://www.baidu.com/s?ie=utf-8&rn=20&wd=',
+	'proxy'=>false,
+	'beautyFunc'=>'baiduBeauty',
+	'userAgent'=>'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+];
 			
 function baiduBeauty($html){
-	$html = '<base href="https://www.baidu.com/" target="_blank" />'.$html;
-	$html = str_replace('<form id="form" name="f" action="/s" class="fm">', '<form id="form" name="f" action="" class="fm"><input type="hidden" name="engine" value="baidu">', $html);
-	$html = str_replace('<input id="kw" name="wd" class="s_ipt" value="', '<input id="kw" name="q" class="s_ipt" value="', $html);
-	$html = preg_replace('/<meta.*?name="referrer".*?>/','<meta content="no-referrer" name="referrer">',$html);
-	return str_replace('<div id="content_left">',  '<div id="content_left" style="padding-left:12px;">', $html);
+	$html = '<base href="https://www.baidu.com/" target="_blank" />' . $html;
+	$html = str_replace('<input id="kw" name="wd" class="s_ipt" value="', '<input id="kw" disabled="disabled" style="cursor: not-allowed;" name="wd" class="s_ipt" value="', $html);
+	$html = str_replace('<input type="submit"', '<input type="submit" disabled="disabled" style="cursor: not-allowed;"', $html);
+	$html = preg_replace('/<meta.*?name="referrer".*?>/','<meta content="no-referrer" name="referrer">', $html);
+	$html = str_replace('<div id="content_left">',  '<div id="content_left" style="padding-left:12px;">', $html);
+	return $html;
 }
 
-$googleConf = ['url'=>'https://www.google.com/search?site=webhp&source=hp&newwindow=1&hl=zh-Hans&num=20&q=',
-			'proxy'=>true,
-			'proxyHost'=>'127.0.0.1',
-			'proxyPort'=>8080,
-			'proxyType'=>'http',
-			'beautyFunc'=>'googleBeauty',
-			'userAgent'=>'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'];
+$googleConf = [
+	'url'=>'https://www.google.com/search?site=webhp&source=hp&newwindow=1&hl=zh-Hans&num=20&q=',
+	'proxy'=>false,
+	//'proxyHost'=>'127.0.0.1',
+	//'proxyPort'=>8080,
+	//'proxyType'=>'http',
+	'beautyFunc'=>'googleBeauty',
+	'userAgent'=>'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+];
 
 function googleBeauty($html){
 	// Change the domain of this link below if your Google url is not ".com"
 	$html = '<base href="https://www.google.com/" target="_blank" />'.$html;
-	$html = preg_replace('/<form([^>]*)action="[^"]*"([^>]*)>/', '<form${1}action=""${2}><input type="hidden" name="engine" value="google">', $html);
-	$html = str_replace('<div class="sbibtd">', '<div class="sbibtd" style="width:500px;">', $html);
-	$html = preg_replace('/onmousedown="[^"]*"/','',$html); // links
 	$html = preg_replace_callback('/<a href="\/url\\?q=([^"]*?)&amp;[^"]*?"/', 
 		function ($matches){
 			return '<a href="' . urldecode($matches[1]) . '"';
 		}, $html);
 	$html = preg_replace('/<meta.*?name="referrer".*?>/','<meta content="no-referrer" name="referrer">',$html);
-	return str_replace('<div id="center_col">', '<div id="center_col" style="margin:0;">', $html);
+	$html = str_replace('<div class="RNNXgb" jsname="RNNXgb"', '<div class="RNNXgb" jsname="RNNXgb" style="width:500px;"', $html);
+	$html = preg_replace('/<button ([^>]*)type="submit"/', '<button ${1}type="submit" disabled="disabled" style="cursor: not-allowed;"', $html);
+	$html = preg_replace('/<input ([^>]*)type="text"/', '<input ${1}type="text" disabled="disabled" style="cursor: not-allowed;"', $html);
+	$html = str_replace('<div id="center_col">', '<div id="center_col" style="margin:0;">', $html);
+	return $html;
 }
